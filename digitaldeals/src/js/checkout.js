4631580d8f57478233944ef3239ae7c4979becae
esmat <138492416+esmattest@users.noreply.github.com>
2023-08-24 22:17:53 +0200

    Add files via upload

--- digitaldeals/src/js/checkout.js
+++ digitaldeals/src/js/checkout.js
@@ -1,4 +1,5 @@
 import { cart } from "./cart.js";
+import { renderTotal } from "./cartoverview.js";
 import { products } from "./products.js";
 
 const checkoutProducts = document.querySelector(".checkout-lists");
@@ -14,24 +15,32 @@ const renderCheckoutProducts = () => {
                 const storageGB = cartProduct.storage / (1024 * 1024 * 1024);
                 return `
                 <div class="card">
-                    <div class="card-image">
-                        <img src="${imagePath}${cartProduct.image}" alt="${cartProduct.name}" />
-                    </div>
-                    <div class="item_details">
+                <div class="card-image">
+                    <img src="${imagePath}${cartProduct.image}" alt="${
+                    cartProduct.name
+                }" />
+                </div>
+                <div class="_details">
+                    <div class="product-info">
                         <h4>${cartProduct.name}</h4>
                         <h5>${storageGB} GB</h5>
-                        <div class="card-price">${cartProduct.price}€</div>
-                        <div class="qty">
-                            <span class="desincrement" data-id="${cartEntry.productId}">-</span>
-                            <strong>${cartEntry.qty}</strong>
-                            <span class="increment" data-id="${cartEntry.productId}">+</span>
-                        </div>
+                        <div class="card-price">${cartProduct.price.toFixed(
+                            2
+                        )}€</div>
+                        <br>
+                        <strong class=productQty>produktanzahl: ${
+                            cartEntry.qty
+                        }</strong>
                     </div>
-                </div>`;
+                    <br>
+                </div>
+            </div>`;
             }
             return "";
         })
         .join("");
+    renderTotal();
+    console.log("renderCheckoutProducts finished");
 };
 
 export { renderCheckoutProducts };
